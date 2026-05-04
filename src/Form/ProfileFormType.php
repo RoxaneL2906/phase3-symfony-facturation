8279474dbe7b019c941732fc58b84dc647307605
Roxane Lelievre <x-roxane.lelievre@hotmail.fr>
2026-05-04 11:48:14 +0200

    US2 : page profil utilisateur avec sidebar

---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('companyName', TextType::class, [
+                'label' => 'Raison sociale',
+            ])
+            ->add('iban', TextType::class, [
+                'label' => 'IBAN',
+            ])
+            ->add('siret', TextType::class, [
+                'label' => 'SIRET',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
